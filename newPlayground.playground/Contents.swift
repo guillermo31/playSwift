6c17fc522d3e46cc53fadcdf2e324df73199bfb1
Guillermo <memo33101@gmail.com>
2018-10-01 09:12:17 -0600

    created a class and a struct

--- newPlayground.playground/Contents.swift
+++ newPlayground.playground/Contents.swift
@@ -1,5 +1,41 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
 
-var str = "Hello, playground"
+
+let streets = ["albermale", "brandywine", "chesapeake"]
+
+
+for street in streets
+{
+    print("I don't live in \(street)")
+}
+
+
+
+class FirstClass
+{
+    func greeting(name: String, age: Int) -> String
+    {
+        return "Hi there \(name), you are \(age)"
+    }
+    
+}
+
+
+var insClass = FirstClass()
+print(insClass.greeting(name: "guillermo", age: 17))
+
+
+struct User
+{
+    var username: String
+    var email: String
+    var name: String
+}
+
+var user = User(username: "gram2068", email: "memo33101@gmail.com", name: "Guillermo Ramos")
+
+print(user.name)
+
+var secClass = FirstClass()
+print(secClass.greeting(name: user.name, age: 17))
